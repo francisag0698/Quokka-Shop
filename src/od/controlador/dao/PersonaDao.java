2771c248c24d26aa7442f49dc36c7b10e3393b65
francisco <40868470+francisag0698@users.noreply.github.com>
2018-07-31 10:40:30 -0500

    Daos añadidos
    Se añadio las clases CuentaDao, PersonaDao

---
+++ src/od/controlador/dao/PersonaDao.java
@@ -0,0 +1,51 @@
+
+import java.io.IOException;
+import od.controlador.Conexion;
+import od.controlador.dao.AdaptadorDao;
+import od.modelo.Persona;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Francis
+ */
+public class PersonaDao extends AdaptadorDao<Persona> {
+
+    private Persona persona;
+
+    public PersonaDao() {
+        super(new Conexion(), Persona.class);
+    }
+
+    public Persona getPersona() {
+        if (persona == null) {
+            persona = new Persona();
+        }
+        return persona;
+    }
+
+    public void fijarInstancia(Persona persona) {
+        this.persona = persona;
+    }
+
+    public boolean guardar() {
+        boolean band = false;
+        try {
+            if (persona.getId() != null) {
+                
+            } else {
+                this.persona.setId(generarID());
+                this.guardar(persona);
+            }
+            band = true;
+        } catch (IOException e) {
+            System.out.println("No se pudo guardar " + e);
+        }
+        return band;
+    }
+
+}
