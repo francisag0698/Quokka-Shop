b94e21f6a6b7ed2233b6f3f17025af37ae989f8b
francisco <40868470+francisag0698@users.noreply.github.com>
2019-01-01 19:47:05 -0500

    inicio del proyecto

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+//Settings
+const { db } = require('./database');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+
+
+//Server Settings
+app.use(function(req, res, next) {
+  next(errors(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
